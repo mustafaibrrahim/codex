21c525b12d5f6c096056c3bccdb252c150f2000d
Mustafa Ibrahim <mustafaibrrahim2511@gmail.com>
2023-03-17 23:57:13 +0200

    delpoy v2

--- server/server.js
+++ server/server.js
@@ -1,17 +1,15 @@
-import express from 'express';
+import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
-
-const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
+import { Configuration, OpenAIApi } from 'openai'
 
 dotenv.config()
-
-console.log(process.env.OPENAI_API_KEY)
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+const openai = new OpenAIApi(configuration);
 
 const app = express()
 app.use(cors())
@@ -47,4 +45,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
